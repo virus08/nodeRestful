0f1f4d0517dcba3a68a2f01de01d134f076b64e3
Wanchai Fuangmali <Wanchai@vstecs.co.th>
2019-01-08 13:12:14 +0700

    npm install

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 'use strict';
-const Hapi = require('hapi');
+
+const Hapi=require('hapi');
 
 // Create a server with a host and port
-const server = new Hapi.Server();
-server.connection({ 
-    host: 'node1.vstecs.local', 
-    port: 8000 
+const server=Hapi.server({
+    host:'localhost',
+    port:8000
 });
 
 // Add the route
 server.route({
-    method: 'GET',
-    path:'/helloworld', 
-    handler: function (request, reply) {
-    return reply('hello world');
-}
+    method:'GET',
+    path:'/hello',
+    handler:function(request,h) {
+
+        return'hello world';
+    }
 });
 
-server.start((err) => {
-   if (err) {
-     throw err;
-   }
-  console.log('Server running at:', server.info.uri);
-});
+// Start the server
+const start =  async function() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
